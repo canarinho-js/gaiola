f89eed675886955c48786e9a28a87e19bc752db8
Hildor Junior <hildorjunior@gmail.com>
2020-06-11 02:25:46 -0300

    get to post

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const cors = require('cors')
+const cors = require('cors');
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 const Sandbox = require('sandbox')
 const sandbox = new Sandbox()
@@ -8,6 +9,7 @@ const sandbox = new Sandbox()
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/saida', (req, res) => {
   sandbox.run(req.body, saida => {
